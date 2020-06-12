4a61ff1053d4c6a13659e296ca91a76dc9aa2647
JackBowln <diasv3861@gmail.com>
2020-06-12 02:57:57 -0300

    sintaxe JSX e imports

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,8 +1,21 @@
 import ReactDOM from'react-dom'
+import React from 'react';
+
+// import ReactDOM from'react-dom'        (importação da DOM, ou Document object model, do react.
+//                                          Isso possibilita a renderização, ReactDOM.render() de um código jsx na nossa aplicação)
+// import React from 'react';              (O isso nos propicia trabalhar com o react na aplicação javascript)
+
+const tag = <strong>Olá React!</strong>
 
 ReactDOM.render(
-    
-    'Olá react!!!',
+
+        <div>
+
+            {tag}                              
+            {/* A sintaxe JSX (javascript react) se apresenta quando usamos 
+                uma sintaxe muito parecida do html mas dentro de um código javascript */}
+
+        </div>,
     document.getElementById('root')
 
     )
