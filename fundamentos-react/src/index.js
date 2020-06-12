86ede3ab781892da5326be66a73a43bb9a980280
JackBowln <diasv3861@gmail.com>
2020-06-12 03:06:52 -0300

    Criação da pasta index.css e linkagem com o index.js

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,9 +1,12 @@
 import ReactDOM from'react-dom'
 import React from 'react';
+import './index.css'
 
 // import ReactDOM from'react-dom'        (importação da DOM, ou Document object model, do react.
 //                                          Isso possibilita a renderização, ReactDOM.render() de um código jsx na nossa aplicação)
 // import React from 'react';              (O isso nos propicia trabalhar com o react na aplicação javascript)
+// import './index.css'                    (O fato de um arquivo css ser criado não o faz ser linkado com o JS, é necessário referencia-lo)
+
 
 const tag = <strong>Olá React!</strong>
 
