f32873c0854cf0ad6ee0a318e6e68f46cf91bf7e
JackBowln <diasv3861@gmail.com>
2020-06-14 03:14:42 -0300

    Estudos sobre componentes básicos, com JSX e variável

--- fundamentos-react/src/components/basicos/Primeiro.js
+++ fundamentos-react/src/components/basicos/Primeiro.js
@@ -1,7 +1,13 @@
 import React from 'react'; //desncessário nesse arquivo, pois não há react aqui, mas caso houvesse algo relacionado ao react precisaria de um import
 
 function Primeiro() {
-    return 'Primeiro Componente'
+    const msg = "Seja bem vindo(a)"
+    return (
+    <div>
+        <h2> Primeiro Componente JSX</h2>
+    <p>{msg}</p>                                   {/*"chaves" indicam um fragmento de javascript dentro do react, como uma variável.*/}
+    </div>
+    )
 }
 
 export default Primeiro
