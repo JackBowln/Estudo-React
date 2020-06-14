4c147dec0a3c8fa8eb60d7a2316161a3fec0a963
JackBowln <diasv3861@gmail.com>
2020-06-14 03:06:58 -0300

    Estudos sobre componentes, sem JSX

---
+++ fundamentos-react/src/components/basicos/Primeiro.js
@@ -0,0 +1,7 @@
+import React from 'react'; //desncessário nesse arquivo, pois não há react aqui, mas caso houvesse algo relacionado ao react precisaria de um import
+
+function Primeiro() {
+    return 'Primeiro Componente'
+}
+
+export default Primeiro
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,23 +1,20 @@
 import ReactDOM from'react-dom'
 import React from 'react';
 import './index.css'
+import Primeiro from './components/basicos/Primeiro'
 
 // import ReactDOM from'react-dom'        (importação da DOM, ou Document object model, do react.
 //                                          Isso possibilita a renderização, ReactDOM.render() de um código jsx na nossa aplicação)
+
 // import React from 'react';              (O isso nos propicia trabalhar com o react na aplicação javascript)
+
 // import './index.css'                    (O fato de um arquivo css ser criado não o faz ser linkado com o JS, é necessário referencia-lo)
 
-
-const tag = <strong>Olá React!</strong>
-
+//import Primeiro from './components/basicos/Primeiro'      import da função "Primeiro.js" da pasta components
 ReactDOM.render(
 
         <div>
-
-            {tag}                              
-            {/* A sintaxe JSX (javascript react) se apresenta quando usamos 
-                uma sintaxe muito parecida do html mas dentro de um código javascript */}
-
+            <Primeiro></Primeiro>
         </div>,
     document.getElementById('root')
 
