0f691227ea1cf9344abc903bfd65c13d98cafd63
JackBowln <diasv3861@gmail.com>
2020-06-14 13:43:22 -0300

    Organização dos cards em flex box pt2

--- fundamentos-react/src/app.jsx
+++ fundamentos-react/src/app.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './app.css'
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
-import Card from './components/Layout/Card'
+import Card from "./components/Layout/Card";
 // import ReactDOM from'react-dom'        (importação da DOM, ou Document object model, do react.
 //                                          Isso possibilita a renderização, ReactDOM.render() de um código jsx na nossa aplicação)
 
@@ -14,32 +15,28 @@ import Card from './components/Layout/Card'
 //import Primeiro from './components/basicos/Primeiro'      import da função "Primeiro.js" da pasta components
 
 export default (props) => (
-  <div id="app">
-      <h1>Fundamentos React</h1>
-    <Card 
-    titulo = 'Desafio Aleatório'>
-          <Aleatorio min={1} max={60}></Aleatorio>
-    </Card>
+  <div className="app">
+    <h1>Fundamentos React</h1>
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={1} max={60}></Aleatorio>
+      </Card>
 
-    <Card 
-    titulo = '#03 - Fragmento'>
-          <Fragmento></Fragmento>
-    </Card>
-    <Card 
-    titulo = '#02 - Com Parametro'>
-          <ComParametro
-      titulo="Situação do aluno" //Todos os componentes devem ser envolvidos por tag
-      aluno="Vinicius"
-      nota={9.5}
-      materia="física"
-    />
-    </Card>
-    <Card 
-    titulo = '#01 - Primeiro'>
-          <Primeiro></Primeiro>
-    </Card>
-    <Card 
-    titulo = 'um título aí'>
-    </Card>
+      <Card titulo="#03 - Fragmento">
+        <Fragmento></Fragmento>
+      </Card>
+      <Card titulo="#02 - Com Parametro">
+        <ComParametro
+          titulo="Situação do aluno" //Todos os componentes devem ser envolvidos por tag
+          aluno="Vinicius"
+          nota={9.5}
+          materia="física"
+        />
+      </Card>
+      <Card titulo="#01 - Primeiro">
+        <Primeiro></Primeiro>
+      </Card>
+      <Card titulo="um título aí"></Card>
+    </div>
   </div>
 );
