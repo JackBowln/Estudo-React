4d48ce9fb74f56f34b3b119c1c0925c52ea620bd
JackBowln <diasv3861@gmail.com>
2020-07-03 19:48:20 -0300

    botões e básico da lógica

--- calculadora/src/Main/Calculator.jsx
+++ calculadora/src/Main/Calculator.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import Button from '../components/Button'
 import Display from '../components/Display'
-const DivCalculator = styled.div `
+const DivCalculator = styled.div`
     height: 320px;
     width: 235px;
     border-radius: 5px;
@@ -13,27 +13,44 @@ const DivCalculator = styled.div `
 `
 
 export default class Calculator extends Component {
-    render(){
-        return(
+    constructor(props){
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+    clearMemory() {
+        console.log('limpar')
+    }
+    setOperation(operation) {
+        console.log(operation)
+    }
+    addDigit(n){
+        console.log(n)
+    }
+    render() {
+        const addDigit = n => this.addDigit(n)
+        const setOperation = op => this.setOperation(op)
+        return (
             <DivCalculator>
                 <Display value={100}></Display>
-                <Button label="AC"></Button>
-                <Button label="/"></Button>
-                <Button label="7"></Button>
-                <Button label="8"></Button>
-                <Button label="9"></Button>
-                <Button label="*"></Button>
-                <Button label="4"></Button>
-                <Button label="5"></Button>
-                <Button label="6"></Button>
-                <Button label="-"></Button>
-                <Button label="1"></Button>
-                <Button label="2"></Button>
-                <Button label="3"></Button>
-                <Button label="+"></Button>
-                <Button className={'doubleButton'} label="0" ></Button>
-                <Button label="."></Button>
-                <Button label="="></Button>
+                <Button triple label="AC" click={this.clearMemory}></Button>
+                <Button operation label="/" click = {this.setOperation}></Button>
+                <Button label="7" click = {this.addDigit}></Button>
+                <Button label="8" click = {this.addDigit}></Button>
+                <Button label="9"  click = {this.addDigit}></Button>
+                <Button operation label="*" click = {this.setOperation}></Button>
+                <Button label="4" click = {this.addDigit}></Button>
+                <Button label="5" click = {this.addDigit}></Button>
+                <Button label="6" click = {this.addDigit}></Button>
+                <Button operation label="-"  click = {this.setOperation}></Button>
+                <Button label="1" click = {this.addDigit}></Button>
+                <Button label="2"  click = {this.addDigit}></Button>
+                <Button label="3" click = {this.addDigit}></Button>
+                <Button operation label="+" click = {this.setOperation}></Button>
+                <Button double label="0" click = {this.addDigit} ></Button>
+                <Button label="." click = {this.addDigit}></Button>
+                <Button operation label="=" click = {this.setOperation}></Button>
             </DivCalculator>
         )
     }
--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -2,28 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import './button.css'
 
-const bgbutton = "#f0f0f0"
-const borderButton = "solid 1px #888"
-const Button = styled.button`
-    
-    font-size: 1.4rem;
-    background-color: ${bgbutton};
-    border: none;
-    border-right: ${borderButton};
-    border-bottom: ${borderButton};
-    outline: none;
-    :active{
-        background-color: #ccc;
-    }
-    &.doubleButton{
-        grid-column: span 2;
-        background-color: red;
-    }
-    `
-
-
 export default props =>{
-    return(
-            <Button>{props.label}</Button>
+    return(<>
+            <button 
+            onClick={e => props.click && props.click(props.label)
+            }
+            className={`
+            button 
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+            `}>{props.label}</button>
+            </>
         )
 }
